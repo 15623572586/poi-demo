d0778d552d582246c5f32104aacb7d2a699ff779
1336800945@qq.com <1336800945@qq.com>
2022-10-09 21:28:07 +0800

    反射

---
+++ src/main/java/com/ontoweb/pois/GenCreateSql.java
@@ -0,0 +1,46 @@
+package com.ontoweb.pois;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class GenCreateSql {
+    public static void main(String[] args) {
+        Workbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook(new FileInputStream("D:\\项目文件\\疾控\\智能报告系统\\问卷数据库字段.xlsx"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+        Row row = sheet.getRow(0);
+        Row row1 = sheet.getRow(1);
+        int num = row.getLastCellNum();
+        StringBuilder sb = new StringBuilder();
+        sb.append("CREATE TABLE cdc_questionnaire(\n" +
+                "`id` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键',\n" +
+//                "PRIMARY KEY (`id`) USING BTREE," +
+                "`create_time` datetime COMMENT '创建时间',\n" +
+                "`update_time` datetime COMMENT '更新时间',\n" +
+                "`create_by` varchar(50) COMMENT '创建人',\n" +
+                "`update_by` varchar(50) COMMENT '更新人',\n");
+        for (int i = 0; i < num; i++) {
+            Cell cell = row.getCell(i);
+            Cell cell1 = row1.getCell(i);
+            String col = cell.getStringCellValue();
+            String col1 = cell1.getStringCellValue();
+            sb.append("`").append(col).append("` VARCHAR(100) COMMENT '"+col1+"'");
+            if(i < num-1) sb.append(",\n");
+        }
+        sb.append(")");
+        System.out.println(sb);
+    }
+}
---
+++ src/main/java/com/ontoweb/pois/GenEntityList.java
@@ -0,0 +1,93 @@
+package com.ontoweb.pois;
+
+import com.ontoweb.pois.utils.StringUtils;
+import lombok.Data;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class GenEntityList {
+
+    public static List<?> reflectionDemo() throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+
+        HashMap<String, Integer> fieldsMap = new HashMap<String, Integer>(){
+            {
+                put("id", 0);
+                put("username", 1);
+                put("birthDate", 2);
+                put("age", 3);
+            }
+        };
+
+        List<List<String>> data = new ArrayList<List<String>>(){
+            {
+                add(new ArrayList<String>(){
+                    {
+                        add("001");
+                        add("yuyunhu");
+                        add("980225");
+                    }
+                });
+                add(new ArrayList<String>(){
+                    {
+                        add("002");
+                        add("zhangjinlong");
+                        add("000000");
+                        add("22");
+                    }
+                });
+            }
+        };
+        List<Object> entities = new ArrayList<>();
+        for (List<String> list: data) {
+            Class<?> clazz = Class.forName("com.ontoweb.pois.Entity");
+            Object entity = clazz.newInstance();
+            for(String key : fieldsMap.keySet()) {
+                Field field = null;  // 获取私有属性字段
+                try {
+                    field = clazz.getDeclaredField(key);
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                }
+                field.setAccessible(true);  // 设置私有属性修改权限
+                Integer col = fieldsMap.get(key);  // 获取列
+                if(col > list.size() - 1) continue;  //  不在列范围内
+                String value = list.get(col);  // 获取col列的值
+                if (StringUtils.isNotEmpty(value)) {  // 如果数据不为空设置字段值
+                    try {
+                        field.set(entity, value);
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+            entities.add(entity);
+        }
+        return entities;
+    }
+
+    public static List<String> getFieldsDemo() throws ClassNotFoundException {
+        Class<?> entityClass = Class.forName("com.ontoweb.pois.Entity");
+        Field[] fields = entityClass.getDeclaredFields();  // 私有属性
+        List<String> fs = new ArrayList<>();
+        for(Field field: fields){
+            fs.add(field.getName());
+        }
+        return fs;
+    }
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        System.out.println(reflectionDemo());
+        System.out.println(getFieldsDemo());
+    }
+
+}
+
+@Data
+class Entity {
+    private String id;
+    private String username;
+    private String birthDate;
+    private String age;
+}
